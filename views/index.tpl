2d5e872bfa43f662d8d96094aafb9ac46d5769d3
xlzd <i@xlzd.me>
2015-10-15 13:26:49 +0800

    index

--- views/index.tpl
+++ views/index.tpl
@@ -1,9 +1,22 @@
-<html>
-    <head>
-    <title>微信公众号搜索 - xlzd</title>
-    </head>
-    <body>
-    <h1>hello world!</h1>
-    <h2>hello world!</h2>
-    </body>
+<!doctype html>
+<html lang="zh">
+<head>
+<meta charset="utf-8" />
+	<title>{{ title }}</title>
+	<link rel="stylesheet" type="text/css" href="static/css/index.css" />
+</head>
+
+<body> 
+		<div id="main">
+			<div id="logo">
+				xlzd
+			</div>
+
+			<div id="search">
+				<input type="text" name="key" id="key" /><br/>
+				<input type="button" name="search" id="search_btn" value="search"/>
+				<input type="button" name="search" id="lucky_btn" value="lucky"/>
+			</div>
+		</div> 
+</body>
 </html>
--- wechat_server.py
+++ wechat_server.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python
+#encoding=utf-8
+
 from bottle import route, run, template, view, static_file
 
 
-@route('/static/<file_name>')
-def route_static_file(file_name):
-	return static_file(file_name, root='static/')
+@route('/static/<tp>/<file_name>')
+def route_static_file(tp, file_name):
+    print tp, file_name
+    return static_file(file_name, root='static/%s/' % tp)
 
 @route('/')
 @view('index')
 def index():
-    return {}
+    return {'title': 'title'}
 
 
 @route('/search/<key>')
@@ -20,3 +24,4 @@ if __name__ == '__main__':
 	PORT = 9132
 	WORKERS = 2
 	run(host='0.0.0.0', port=PORT, server='gunicorn', workers=WORKERS)
+	# run(host='0.0.0.0', port=PORT)
